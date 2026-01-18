f1c42b738f9adcafaeb4f0648b008c95f4bed230
tomo-john <keririnkery.kery@gmail.com>
2026-01-19 07:20:33 +0900

    🐶 chore: Livewireテスト

--- resources/views/pages/settings/⚡profile.blade.php
+++ resources/views/pages/settings/⚡profile.blade.php
@@ -83,7 +83,8 @@ new class extends Component {
 
     <x-pages::settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
-            <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
+            <flux:input wire:model.live="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
+            <div>入力された名前は: {{ $name }}</div>
 
             <div>
                 <flux:input wire:model="email" :label="__('Email')" type="email" required autocomplete="email" />

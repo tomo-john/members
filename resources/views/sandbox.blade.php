3a1187ce3fca531f2f1e586e07948788be95ca71
tomo-john <keririnkery.kery@gmail.com>
2026-01-21 12:52:49 +0900

    🐶 chore: sandboxのスタイル(テスト)

--- resources/views/sandbox.blade.php
+++ resources/views/sandbox.blade.php
@@ -1,2 +1,11 @@
-<h1>Hello 🐶</h1>
-<a href="{{ route('home') }}">HOME</a>
+<x-layouts::app>
+    <h1>Hello 🐶</h1>
+    <a href="{{ route('home') }}">HOME</a>
+    <i class="fa-solid fa-dog"></i>
+    <flux:input
+        type="date"
+        label="日付を選択"
+        name="published_at"
+        view="calendar" {{-- カレンダーアイコンを表示 --}}
+    />
+</x-layouts::app>

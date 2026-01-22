aa7f856fca2ed4a20bf86cd8b41ebced47db9c46
tomo-john <keririnkery.kery@gmail.com>
2026-01-23 07:08:20 +0900

    🐶 chore: ドキュメント追加・スタイル調整

--- resources/views/sandbox.blade.php
+++ resources/views/sandbox.blade.php
@@ -1,6 +1,6 @@
 <x-layouts::app>
     <div class="bg-gray-800">
-        <h1 class="mx-4">Hello 🐶</h1>
+        <h2 class="text-2xl text-white font-semibold m-4">Snad Box<i class="fa-solid fa-dog ml-2"></i><h2>
 
         <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <x-message :message="session('message')" type="success" />
@@ -14,9 +14,22 @@
         </div>
     </div>
 
+    <!-- Livewire Volt -->
     <div class="bg-gray-800 mt-6">
+        <h2 class="text-2xl text-white font-semibold m-4">Volt<i class="fa-solid fa-bolt ml-2"></i><h2>
+
         <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <livewire:sandbox-lookup />
         </div>
     </div>
+
+    <!-- ドキュメントエリア -->
+    <div class="bg-gray-800 mt-6">
+        <h2 class="text-2xl text-white font-semibold m-4">Memo<i class="fa-solid fa-pen ml-2"></i><h2>
+        <div class="m-4 space-y-2">
+            <p>index, storeは通常のController</p>
+            <p>Livewire(Volt)でリアルタイム表示 + 更新処理</p>
+        </div>
+    </div>
+
 </x-layouts::app>

171a7f97be2b29d12c4c654a65a6dc29858227b0
tomo-john <keririnkery.kery@gmail.com>
2026-01-23 19:33:40 +0900

    🐶 chore: 保存処理(途中)

--- app/Models/Dog.php
+++ app/Models/Dog.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dog extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'birthday',
+        'is_good_boy',
+    ];
+
+    protected $casts = [
+        'birthday' => 'date',
+        'is_good_boy' => 'boolean',
+    ];
 }
--- resources/views/components/⚡dog-manager.blade.php
+++ resources/views/components/⚡dog-manager.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use Livewire\Component;
+use App\Models\Dog;
 
 new class extends Component
 {
@@ -10,14 +11,33 @@ new class extends Component
 
     public function save()
     {
-        dd($this->name);
+        $validated = $this->validate([
+            'name' => 'required|string|max:50',
+            'birthday' => 'nullable|date',
+            'is_good_boy' => 'boolean',
+        ]);
+
+        Dog::create($validated);
+
+        $this->reset([
+            'name',
+            'birthday',
+            'is_good_boy'
+        ]);
     }
 };
 ?>
 
 <div>
-    <h2>Hello🐶</h2>
-    <p><i class="fa-solid fa-dog"></i></p>
-    <flux:input label="Dog name" wire:model="name" />
-    <flux:button wire:click="save">保存</flux:button>
+    <h2 class="text-2xl text-white font-semibold">
+        Dog
+        <i class="fa-solid fa-dog ml-2"></i>
+    </h2>
+
+    <div class="max-w-2xl mx-auto space-y-4 border rounded-2xl mt-6 p-6">
+        <flux:input label="Dog name" wire:model="name" placeholder="例: じょん" />
+        <flux:input label="Birthday" wire:model="birthday" type="date" />
+        <flux:checkbox label="Good Boy? 🐶" wire:model="is_good_boy" />
+        <flux:button wire:click="save">保存</flux:button>
+    </div>
 </div>

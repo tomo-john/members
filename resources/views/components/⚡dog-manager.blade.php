41cb406d6b6e2f0eecee3666ce79aae1a8511ffb
tomo-john <keririnkery.kery@gmail.com>
2026-01-23 13:25:05 +0900

    🐶 chore: wire:model

--- resources/views/components/⚡dog-manager.blade.php
+++ resources/views/components/⚡dog-manager.blade.php
@@ -4,11 +4,20 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $name;
+    public $birthday;
+    public $is_good_boy;
+
+    public function save()
+    {
+        dd($this->name);
+    }
 };
 ?>
 
 <div>
     <h2>Hello🐶</h2>
     <p><i class="fa-solid fa-dog"></i></p>
+    <flux:input label="Dog name" wire:model="name" />
+    <flux:button wire:click="save">保存</flux:button>
 </div>
--- resources/views/dogs.blade.php
+++ resources/views/dogs.blade.php
@@ -1,3 +1,3 @@
 <x-layouts::dog>
     <livewire:dog-manager />
-</x-layouts:dog>
+</x-layouts::dog>

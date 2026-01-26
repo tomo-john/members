fc1f0aeb5dcc7612b5c7703a78a7075f40f79df8
tomo-john <keririnkery.kery@gmail.com>
2026-01-27 08:13:53 +0900

    🐶 chore: Taskモデルに$fillableと$casts追加

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'due_date',
+        'is_done',
+        'priority',
+    ];
+
+    protected $casts = [
+        'due_date' => 'date',
+        'is_done' => 'boolean',
+    ];
 }

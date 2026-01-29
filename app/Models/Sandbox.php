54c17ddae654ec9f445888fab0541337c2b33141
tomo-john <keririnkery.kery@gmail.com>
2026-01-30 07:22:18 +0900

    🐶 chore: moodの状態をModelに定義

--- app/Models/Sandbox.php
+++ app/Models/Sandbox.php
@@ -17,4 +17,15 @@ class Sandbox extends Model
         'is_good_boy' => 'boolean',
         'birthday' => 'datetime',
     ];
+
+    // mood
+    public const MOOD_IDLE = 'idle';
+    public const MOOD_HAPPY = 'happy';
+    public const MOOD_SLEEP = 'sleep';
+
+    public const MOODS = [
+        self::MOOD_IDLE,
+        self::MOOD_HAPPY,
+        self::MOOD_SLEEP,
+    ];
 }

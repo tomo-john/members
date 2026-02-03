2a7030ffce1d25af2f0218541b39833a34a891ae
tomo-john <keririnkery.kery@gmail.com>
2026-02-04 08:05:53 +0900

    🐶 chore: Admin用のメール設定

--- config/mail.php
+++ config/mail.php
@@ -114,5 +114,6 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
+    'admin' => env('MAIL_ADMIN', null),
 
 ];

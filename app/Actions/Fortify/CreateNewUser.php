cb7fd839c748ff05d23bb78ed18497679a97426f
tomo-john <keririnkery.kery@gmail.com>
2026-02-06 08:10:06 +0900

    🐰 feat: User登録時のアバター登録処理追加
    - 間違っているかも
    - Userモデルのfillableも追加

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -17,6 +17,8 @@ class CreateNewUser implements CreatesNewUsers
      *
      * @param  array<string, string>  $input
      */
+
+    /** 元のコード
     public function create(array $input): User
     {
         Validator::make($input, [
@@ -29,5 +31,39 @@ class CreateNewUser implements CreatesNewUsers
             'email' => $input['email'],
             'password' => $input['password'],
         ]);
+    }
+     */
+
+    // 作り直し
+    public function create(array $input): User
+    {
+        // 1. バリデーション実行
+        // 成功すると、ルールに定義した項目だけが $validated に入る
+        $validated = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique(User::class)],
+            'password' => $this->passwordRules(),
+            'avatar' => ['nullable', 'image', 'max:1024'],
+        ])->validate();
+
+        // 2. ユーザーデータの準備（ハッシュ化などは自動または手動で確認してね）
+        $userData = [
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => $input['password'], // Fortifyが自動でハッシュ化しない設定なら Hash::make() が必要
+        ];
+
+        // 3. アバター処理
+        // バリデーション後の $validated['avatar'] を使うのが安全！
+        if (isset($input['avatar'])) {
+            $timestamp = now()->format('YmdHis');
+            $originalName = $input['avatar']->getClientOriginalName();
+            $filename = $timestamp . '_' . $originalName;
+
+            $input['avatar']->storeAs('avatar', $filename, 'public');
+            $userData['avatar'] = $filename;
+        }
+
+        return User::create($userData);
     }
 }

b8c05c052b05176bebe6143cb1db3e54164b1152
tomo-john <keririnkery.kery@gmail.com>
2026-02-12 09:16:43 +0900

    🐰 feat: User一覧画面にページネーションを追加

--- app/Livewire/UserList.php
+++ app/Livewire/UserList.php
@@ -5,15 +5,15 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\User;
 use App\Models\Role;
+use Livewire\WithPagination;
 
 class UserList extends Component
 {
-    public $users;
+    use WithPagination;
     public $roles;
 
     public function mount(): void
     {
-        $this->users = User::all();
         $this->roles = Role::all();
     }
 
@@ -21,7 +21,7 @@ class UserList extends Component
     {
         $user = User::findOrFail($id);
         $user->delete();
-        $this->users = $this->users->reject(fn ($user) => $user->id === $id);
+        // $this->users = $this->users->reject(fn ($user) => $user->id === $id);
         session()->flash('message', 'ユーザーを削除しました');
     }
 
@@ -38,6 +38,8 @@ class UserList extends Component
 
     public function render()
     {
-        return view('livewire.user-list');
+        return view('livewire.user-list', [
+            'users' => User::paginate(10)
+        ]);
     }
 }
